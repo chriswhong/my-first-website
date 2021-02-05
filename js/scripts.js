a51cf31d39758f8e70536deb186c7c3cf318a133
Chris Whong <chris.m.whong@gmail.com>
2021-02-04 19:25:01 -0500

    migrated code from my jsfiddle

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
-console.log('Hello, looks like the javascript file is loading properly')
+$('.album-list-item').on('click', function() {
+	// pull out the album cover url from the element's attributes
+	var albumCoverUrl = $(this).attr('data-album-cover')
+  // log the album cover url to the console
+  console.log(albumCoverUrl)
+
+  $('#album-art').empty()
+
+  $('#album-art').css('background-image', `url(${albumCoverUrl})`)
+})
